745c49c90e0b22847c169d5ebecf05aa5e090da0
David Tsai <david@whataniceguy.com>
2018-08-17 13:40:30 -0700

    Create TreeNode class

---
+++ src/components/TreeNode.js
@@ -0,0 +1,7 @@
+export default class TreeNode {
+  constructor(value, leftChild = null, rightChild = null) {
+    this.value = value;
+    this.left = leftChild;
+    this.right = rightChild;
+  }
+}
